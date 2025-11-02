0c3e1efea3bebfa6c8d842cf39ee946ec120d7a6
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-11-02 11:49:12 +0530

    support

--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero Section</h1>
-     );
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 mt-5 mb-5" id="supportWrapper">
+        <h5 className="fs-1 fw-bold">Support Center</h5>
+        <a href="#" style={{ textDecoration: "none", color: "white",textDecoration:"none"}}>
+          Track your ticket status
+        </a>
+      </div>
+      <div className="row p-5 mt-5 mb-5">
+        <div className="col-6 P-5">
+          <h3 className="fs-3 fw-bold">How can we help you?</h3>
+          <p className="fs-5">
+            Find answers to common questions, explore our support articles, or</p>
+        </div>
+        
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
