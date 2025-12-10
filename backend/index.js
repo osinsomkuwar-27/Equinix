5a54b15f79410d5a1f0682d6cc1dbe7cd7e24779
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-10 09:03:43 +0530

    connected mongodb to express

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
 app.listen(3002, () => {
   console.log("Server is running on port 3002");
+  mongoose.connect(uri);
+  console.log("Connected to MongoDB");
 });
